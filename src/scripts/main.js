fa956075df9b3a3be7db8dbc5823a680e966f6d4
BayonLucas <luqueti.3110@gmail.com>
2026-01-27 20:24:38 -0300

    checkpoint: Cuenta regresiva funcional

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -28,6 +28,45 @@ if (window.device === 'mobile' || window.innerWidth < 768) {
     imageInstagramParallax = window.support_format_webp() ? 'img/instagram.webp' : 'img/instagram.jpg';
 }
 
+// 4. Cuenta Regresiva
+// Set the date we're counting down to
+var countDownDate = new Date(fechaCuentaRegresiva).getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    // document.getElementById("reloj").innerHTML = days + " días " + hours + "hs " +  minutes + "m " + seconds + "s ";
+
+    $("#dias .number").text(days);
+    $("#horas .number").text(hours);
+    $("#minutos .number").text(minutes);
+    $("#segundos .number").text(seconds);
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("reloj").innerHTML = '<p class="fin-cuenta">' +
+        lang_textoFinalCuentaRegresiva + '</p>';
+        $('.falta').text('');
+    }
+}, 1000);
+
+
+
+
 // 5. Inicialización de Plugins (Aseguramos que el DOM esté listo)
 document.addEventListener('DOMContentLoaded', () => {
     
